1db09b4bcb4c549c1d7679ff84c1e2ce3436d974
Deep <145989362+acekane@users.noreply.github.com>
2025-03-11 13:02:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddData from './Component/AddData'
+import Display from './Component/Display'
+import MainRoute from './AllRoute/MainRoute'
+
+function App() {
+  
+  return (
+    <>    
+          <MainRoute/>
+         
+          
+           
+
+     </>
+  )
+}
+
+export default App
